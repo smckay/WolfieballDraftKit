bbca71fdad49046972fbefa9c8687285ab866ad2
Your Name Here <sammckay@10.255.4.46>
2015-04-13 01:12:31 -0400

    first commit, WolfieballDraftKit and gui in progress

---
+++ src/gui/gui.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author sammckay
+ */
+public class gui {
+    
+    // THIS IS THE APPLICATION WINDOW
+    Stage primaryStage;
+    
+     // THIS IS THE STAGE'S SCENE GRAPH
+    Scene primaryScene;
+    
+    // THIS PANE ORGANIZES THE BIG PICTURE CONTAINERS FOR THE
+    // APPLICATION GUI
+    BorderPane wbPane;
+    
+    // THIS IS THE TOP TOOLBAR AND ITS CONTROLS
+    FlowPane topToolbarPane;
+    Button newDraftButton;
+    Button loadDraftButton;
+    Button saveDraftButton;
+    Button exportButton;
+    Button exitButton;
+    
+    
+    
+    public gui(Stage initPrimaryStage) {
+        primaryStage = initPrimaryStage;
+    }
+ 
+    public void initGui(){
+    
+            initFileToolbar();
+            
+            initWindow("Wolfieball");
+        
+    }
+    
+    public void initFileToolbar(){};
+    
+    // INITIALIZE THE WINDOW (i.e. STAGE) PUTTING ALL THE CONTROLS
+    // THERE EXCEPT THE WORKSPACE, WHICH WILL BE ADDED THE FIRST
+    // TIME A NEW Course IS CREATED OR LOADED
+    private void initWindow(String windowTitle) {
+        // SET THE WINDOW TITLE
+        primaryStage.setTitle(windowTitle);
+
+        // GET THE SIZE OF THE SCREEN
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+
+        // AND USE IT TO SIZE THE WINDOW
+        primaryStage.setX(bounds.getMinX());
+        primaryStage.setY(bounds.getMinY());
+        primaryStage.setWidth(bounds.getWidth());
+        primaryStage.setHeight(bounds.getHeight());
+
+        // ADD THE TOOLBAR ONLY, NOTE THAT THE WORKSPACE
+        // HAS BEEN CONSTRUCTED, BUT WON'T BE ADDED UNTIL
+        // THE USER STARTS EDITING A COURSE
+        wbPane = new BorderPane();
+        wbPane.setTop(topToolbarPane);
+        primaryScene = new Scene(wbPane);
+
+        // NOW TIE THE SCENE TO THE WINDOW, SELECT THE STYLESHEET
+        // WE'LL USE TO STYLIZE OUR GUI CONTROLS, AND OPEN THE WINDOW
+        //primaryScene.getStylesheets().add(PRIMARY_STYLE_SHEET);
+        primaryStage.setScene(primaryScene);
+        primaryStage.show();
+    }
+    
+}
