a0c8cb15417002ab982217b9208d2b42e52eb70c
Your Name Here <sammckay@172.24.55.244>
2015-04-30 13:55:37 -0400

    hitter class started

---
+++ src/file/JsonFileManager.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package file;
+
+
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.collections.ObservableList;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonReader
+;import javax.json.JsonWriter;
+import javax.json.JsonValue;
+import data.Hitter;
+
+/**
+ *
+ * @author sammckay
+ */
+public class JsonFileManager {
+ 
+      String JSON_EXT = ".json";
+      String SLASH = "/";
+      
+      
+
+    
+    
+    
+}
+
